5f7719f41dcf63ff6780ce53bdedba8bf1db0478
AnkushGauro <ajank1yu@gmail.com>
2024-07-04 22:36:26 -0400

    Add files via upload

---
+++ oop244/MS2/IOAble.h
@@ -0,0 +1,42 @@
+/*
+-----------------------------------------------------------------------------
+ Author      : Ankush Gauro
+ Email       : agauro@myseneca.ca
+ Student Id  : 108593237
+ Section     : ZCC
+ Date		 : 2024-03-17
+ I have done all the coding by myself and only copied the code that my professor provided to complete my workshops and assignments.
+-----------------------------------------------------------------------------
+ */
+
+#ifndef IOABLE_H
+#define IOABLE_H
+#include <iostream>
+
+using namespace std;
+
+namespace seneca{
+    
+    class IOAble{
+
+    public:
+        virtual ostream& write(ostream& os) const = 0;
+        virtual istream& read(istream& is) = 0;
+
+        virtual ostream& csvWrite(ostream& ostr) const{
+            return ostr;
+        }
+
+        virtual istream& csvRead(istream& istr){
+            return istr;
+        }
+
+        virtual ~IOAble() = default;
+    };
+
+    ostream& operator<<(ostream& os, const IOAble& obj);
+    istream& operator>>(istream& is, IOAble& obj);
+
+}
+#endif
+
