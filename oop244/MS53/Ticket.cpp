6763d9bb04b448faa16a8dbc120bccd6e03d41b4
AnkushGauro <ajank1yu@gmail.com>
2024-07-04 22:39:35 -0400

    Add files via upload

---
+++ oop244/MS53/Ticket.cpp
@@ -0,0 +1,62 @@
+/*
+-----------------------------------------------------------------------------
+ Author      : Ankush Gauro
+ Email       : agauro@myseneca.ca
+ Student Id  : 108593237
+ Section     : ZCC
+ Date		 : 2024-04-09
+ I have done all the coding by myself and only copied the code that my professor provided to complete my workshops and assignments.
+-----------------------------------------------------------------------------
+ */
+
+#include <iostream>
+#include "Ticket.h"
+using namespace std;
+
+namespace seneca {
+    Ticket::Ticket(int number) {
+        m_number = number;
+    }
+
+    Time Ticket::time() const {
+        return m_time;
+    }
+
+    int Ticket::number() const {
+        return m_number;
+    }
+
+    void Ticket::resetTime() {
+        m_time.reset();
+    }
+
+    ostream& Ticket::csvWrite(ostream& ostr) const {
+        ostr << m_number << " " << m_time;
+        return ostr;
+    }
+
+    istream& Ticket::csvRead(istream& istr) {
+        char delimiter;
+        istr >> m_number >> delimiter >> m_time;
+        return istr;
+    }
+
+    ostream& Ticket::write(ostream& ostr) const {
+        if (&ostr == &cout) {
+            ostr << "Ticket No: " << m_number << ", Issued at: " << m_time;
+        }
+        else if (&ostr == &clog) {
+            ostr << m_number << " " << m_time;
+        }
+        else {
+            ostr << m_number << "," << m_time;
+        }
+        return ostr;
+    }
+
+    istream& Ticket::read(istream& istr) {
+        istr >> m_number;
+        istr.ignore();
+        return istr >> m_time;
+    }
+}
---
+++ oop244/MS53/ms53_test.cpp
@@ -0,0 +1,57 @@
+/*
+-----------------------------------------------------------------------------
+ Author      : Ankush Gauro
+ Email       : agauro@myseneca.ca
+ Student Id  : 108593237
+ Section     : ZCC
+ Date		 : 2024-04-09
+ I have done all the coding by myself and only copied the code that my professor provided to complete my workshops and assignments.
+-----------------------------------------------------------------------------
+ */
+
+ /***********************************************************************
+ // OOP244 Project
+ //
+ // File  ms53_test.cpp
+ // Author  Fardad Soleimanloo
+ // Description
+ //
+ // Revision History
+ // -----------------------------------------------------------
+ // Name                 Date            Reason
+ ***********************************************************************/
+#include <iostream>
+#include <fstream>
+#include "PreTriage.h"
+#include "Utils.h"
+using namespace std;
+using namespace seneca;
+void copyFile(const char* des, const char* src) {
+    ifstream infile(src);
+    ofstream outfile(des);
+    if (outfile) {
+        char ch;
+        while (infile.get(ch)) {
+            outfile.put(ch);
+        }
+    }
+}
+void displayFile(const char* fname) {
+    ifstream fin(fname);
+    char ch;
+    cout << endl << "******  Content of file: \"" << fname << "\":" << endl;
+    while (fin.get(ch)) cout << ch;
+    cout << "***************************************************" << endl << endl;
+}
+void theApp() {
+    PreTriage P("smalldata.csv");
+    P.run();
+}
+int main() {
+    copyFile("smalldata.csv", "smalldata.csv.bak");
+    seneca::debug = true;
+    U.setDebugTime(8, 41);
+    theApp();
+    displayFile("smalldata.csv");
+    return 0;
+}
