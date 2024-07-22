c5224a0fa2f915bbbbe089ec339ca40e24f33bb4
AnkushGauro <ajank1yu@gmail.com>
2024-07-22 10:31:23 -0400

    Add files via upload

---
+++ Web222/Assignment 5/src/js/artists.js
@@ -0,0 +1,36 @@
+/**
+ * artists.js
+ *
+ * The app's list of Artists
+ */
+
+window.artists = [
+  /* TODO */
+  {
+    artistId: "AID-1",
+    name: "Sushant KC",
+    urls: [
+      { url: "https://www.instagram.com/sushantkcofficial/", name: "Instagram" },
+      {
+        url: "https://open.spotify.com/artist/5R5EUMWsrYDt3ItavrP6C5?si=yIhqpQpDSp-ZFQqMPRgIaA&nd=1&dlsi=eb5df21eec2e48b8",
+        name: "Spotify"
+      }
+    ]
+  },
+  {
+    artistId: "AID-2",
+    name: "Sajjan Raj Vaidya",
+    urls: [
+      { url: "https://www.instagram.com/sajjanrajvaidya/", name: "Instagram" },
+      { url: "https://sajjanrajvaidya.com/", name: "Official Website" }
+    ]
+  },
+  {
+    artistId: "AID-3",
+    name: "Samir Shrestha",
+    urls: [
+      { url: "https://www.instagram.com/samirstha319/?hl=en", name: "Instagram" },
+      { url: "https://www.facebook.com/SamirStha0319/", name: "Facebook" }
+    ]
+  }
+];
---
+++ Web222/Assignment 5/src/js/songs.js
@@ -0,0 +1,208 @@
+/**
+ * songs.js
+ *
+ * The app's songs
+ */
+
+window.songs = [
+  {
+    songId: "SID-1",
+    artistId: "AID-1",
+    title: "Sathi",
+    year: "2019",
+    duration: 222,
+    url: "https://www.youtube.com/watch?v=7aoDZ8UeUxU",
+    imageUrl: "image/image1.jpg",
+    explicit: false
+  },
+  {
+    songId: "SID-2",
+    artistId: "AID-1",
+    title: "Sarangi",
+    year: "2023",
+    duration: 299,
+    url: "https://www.youtube.com/watch?v=Sh8ZYHnb86c",
+    imageUrl: "image/image2.jpg",
+    explicit: false
+  },
+  {
+    songId: "SID-3",
+    artistId: "AID-1",
+    title: "Risaune Bhaye",
+    year: "2022",
+    duration: 199,
+    url: "https://www.youtube.com/watch?v=cNBmzxE6Jf0",
+    imageUrl: "image/image3.jpg",
+    explicit: false
+  },
+  {
+    songId: "SID-4",
+    artistId: "AID-1",
+    title: "Maya Ma",
+    year: "2018",
+    duration: 277,
+    url: "https://www.youtube.com/watch?v=nBfaj_blvB8",
+    imageUrl: "image/image4.jpg",
+    explicit: false
+  },
+  {
+    songId: "SID-5",
+    artistId: "AID-1",
+    title: "Gulabi",
+    year: "2019",
+    duration: 253,
+    url: "https://www.youtube.com/watch?v=QFoPEP-V764",
+    imageUrl: "image/image5.jpg",
+    explicit: false
+  },
+  {
+    songId: "SID-6",
+    artistId: "AID-1",
+    title: "Behos",
+    year: "2018",
+    duration: 218,
+    url: "https://www.youtube.com/watch?v=XbM6PyRsJc4",
+    imageUrl: "image/image1.jpg",
+    explicit: false
+  },
+  {
+    songId: "SID-7",
+    artistId: "AID-1",
+    title: "Muskurayera",
+    year: "2018",
+    duration: 199,
+    url: "https://www.youtube.com/watch?v=nBxsKUhw0MA",
+    imageUrl: "image/image2.jpg",
+    explicit: false
+  },
+  {
+    songId: "SID-8",
+    artistId: "AID-1",
+    title: "Satayera",
+    year: "2018",
+    duration: 224,
+    url: "https://www.youtube.com/watch?v=gzgqGGGV7Jg",
+    imageUrl: "image/image3.jpg",
+    explicit: false
+  },
+  {
+    songId: "SID-9",
+    artistId: "AID-1",
+    title: "Parkha Na",
+    year: "2024",
+    duration: 242,
+    url: "https://www.youtube.com/watch?v=qQujA8u1zGI",
+    imageUrl: "image/image4.jpg",
+    explicit: true
+  },
+  {
+    songId: "SID-10",
+    artistId: "AID-1",
+    title: "Jhyal Bata",
+    year: "2023",
+    duration: 227,
+    url: "https://www.youtube.com/watch?v=6-a_wfFF_Xg",
+    imageUrl: "image/image5.jpg",
+    explicit: true
+  },
+  {
+    songId: "SID-11",
+    artistId: "AID-2",
+    title: "Chitthi Bhitra",
+    year: "2018",
+    duration: 303,
+    url: "https://www.youtube.com/watch?v=GrlAInfP990",
+    imageUrl: "image/image1.jpg",
+    explicit: false
+  },
+  {
+    songId: "SID-12",
+    artistId: "AID-2",
+    title: "Sasto Mutu",
+    year: "2022",
+    duration: 302,
+    url: "https://www.youtube.com/watch?v=XcEC2q4CotY",
+    imageUrl: "image/image2.jpg",
+    explicit: false
+  },
+  {
+    songId: "SID-13",
+    artistId: "AID-2",
+    title: "Hataarindai",
+    year: "2019",
+    duration: 341,
+    url: "https://www.youtube.com/watch?v=Ogr6ygCwRwc",
+    imageUrl: "image/image3.jpg",
+    explicit: false
+  },
+  {
+    songId: "SID-14",
+    artistId: "AID-2",
+    title: "Mooskaan",
+    year: "2018",
+    duration: 315,
+    url: "https://www.youtube.com/watch?v=suUZbb3zYw8",
+    imageUrl: "image/image4.jpg",
+    explicit: false
+  },
+  {
+    songId: "SID-15",
+    artistId: "AID-2",
+    title: "Naganya Maya",
+    year: "2021",
+    duration: 320,
+    url: "https://www.youtube.com/watch?v=csoXwcbmNLw",
+    imageUrl: "image/image5.jpg",
+    explicit: false
+  },
+  {
+    songId: "SID-16",
+    artistId: "AID-3",
+    title: "Thamana Haat",
+    year: "2022",
+    duration: 321,
+    url: "https://www.youtube.com/watch?v=qyRrUEInzAs",
+    imageUrl: "image/image1.jpg",
+    explicit: false
+  },
+  {
+    songId: "SID-17",
+    artistId: "AID-3",
+    title: "Hera Na",
+    year: "2022",
+    duration: 219,
+    url: "https://www.youtube.com/watch?v=hev768NSXJQ",
+    imageUrl: "image/image2.jpg",
+    explicit: false
+  },
+  {
+    songId: "SID-18",
+    artistId: "AID-3",
+    title: "Kuri Basechu",
+    year: "2021",
+    duration: 220,
+    url: "https://www.youtube.com/watch?v=m7agYW8o6oQ",
+    imageUrl: "image/image3.jpg",
+    explicit: false
+  },
+  {
+    songId: "SID-19",
+    artistId: "AID-3",
+    title: "Chaar Dewar",
+    year: "2020",
+    duration: 182,
+    url: "https://www.youtube.com/watch?v=NW63lS546YQ",
+    imageUrl: "image/image4.jpg",
+    explicit: false
+  },
+  {
+    songId: "SID-20",
+    artistId: "AID-3",
+    title: "Maya Ta Maya Ho",
+    year: "2021",
+    duration: 175,
+    url: "https://www.youtube.com/watch?v=WGm31p9oTT0",
+    imageUrl: "image/image5.jpg",
+    explicit: false
+  }
+];
